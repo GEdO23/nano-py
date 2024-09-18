13c0050ce63502f65656f70090c01bebcba1b49d
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-18 17:06:52 -0300

    AULA 2 - Hora da leitura

--- manipulacao_arquivos/meu_primeiro_arquivo.py
+++ manipulacao_arquivos/meu_primeiro_arquivo.py
@@ -1,5 +1,11 @@
-with open('primeiro_arquivo.txt', 'w') as arquivo:
+with open('arquivo.txt', 'w') as arquivo:
     arquivo.write('Hello World!')
 
-with open('primeiro_arquivo.txt', 'a') as arquivo:
+with open('arquivo.txt', 'a') as arquivo:
     arquivo.write('\nHow are you?')
+
+with open("arquivo.txt", "r") as arquivo:
+    # conteudo = arquivo.read()
+    # conteudo = arquivo.readline()
+    for linha in arquivo.readlines():
+        print(linha)
