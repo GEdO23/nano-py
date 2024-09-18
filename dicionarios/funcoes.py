5a432f84b0471b6222fa6633fbb8a269c844a035
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-18 18:20:26 -0300

    Preenchendo e persistindo o dicionario

--- dicionarios/funcoes.py
+++ dicionarios/funcoes.py
@@ -6,12 +6,39 @@ def perguntar():
                  "<L> - Para Listar um usuário: ").upper()
 
 
-def inserir():
-    salvar(input("Digite o login: ").upper(), [input("Digite o nome: ").upper(),
-                                               input("Digite a última data de acesso: "),
-                                               input("Qual a última estação acessada: ").upper()])
+def preencher_dicionario(dicionario):
+    with (open('bd.txt', 'r') as arquivo):
+        # Dividir em uma lista de valor e chave com strip
+        for linha in arquivo:
+            usuario = linha.split(':')
+            lista = formatar_lista_de_arquivo(usuario[1])
+            dicionario[usuario[0]] = lista
+
+
+def formatar_lista_de_arquivo(usuario: str):
+    usuario = usuario.replace("[", "")
+    usuario = usuario.replace("]", "")
+    usuario = usuario.replace("'", "")
+    usuario = usuario.replace(" ", "")
+    usuario = usuario.replace("\n", "")
+    usuario = usuario.split(",")
+    return usuario
+
+
+def inserir(dicionario):
+    login = input("Digite o login: ").upper()
+    usuario = [input("Digite o nome: ").upper(),
+               input("Digite a última data de acesso: "),
+               input("Qual a última estação acessada: ").upper()]
+    dicionario[login] = usuario
+    salvar(login, usuario)
 
 
 def salvar(chave, valor):
     with open("bd.txt", "a") as arquivo:
         arquivo.write(chave + ":" + str(valor) + "\n")
+
+# def pesquisar(dicionario):
+#     with open("bd.txt", "r") as arquivo:
+#         for linha in arquivo:
+#             if (linha.strip()):
