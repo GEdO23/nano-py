c55fd19e01e9703c7746dab5c470e20565c6143b
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-20 23:45:58 -0300

    AULA 5 - Conversando com SO - platform

---
+++ sistema_operacional/so_teste.py
@@ -0,0 +1,8 @@
+import platform
+
+print("Nome maquina:..........", platform.node())
+print("Arquitetura:...........", platform.architecture())
+print("Sistema Operacional:...", platform.system())
+print("Versao do SO:..........", platform.release())
+print("Processador:...........", platform.processor())
+print("Versao do Python:......", platform.python_version())
