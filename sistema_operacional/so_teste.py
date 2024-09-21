4d5585c1bca9004e8c634b420c41f17b943fd13f
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-20 23:47:57 -0300

    AULA 5 - Conversando com SO - datetime

--- sistema_operacional/so_teste.py
+++ sistema_operacional/so_teste.py
@@ -1,4 +1,5 @@
 import platform
+from datetime import datetime
 
 print("Nome maquina:..........", platform.node())
 print("Arquitetura:...........", platform.architecture())
@@ -6,3 +7,8 @@ print("Sistema Operacional:...", platform.system())
 print("Versao do SO:..........", platform.release())
 print("Processador:...........", platform.processor())
 print("Versao do Python:......", platform.python_version())
+
+print("\nAgora:...", datetime.now())
+print("Ano:.....", datetime.now().year)
+print("Hora:....", datetime.now().hour)
+print("Mes:.....", datetime.now().month)
