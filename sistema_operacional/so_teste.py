f28de8189e220a3c83f2ca3688487f313a34ccea
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-21 00:00:17 -0300

    AULA 5 - Conversando com SO - getpass

--- sistema_operacional/so_teste.py
+++ sistema_operacional/so_teste.py
@@ -1,14 +1,24 @@
 import platform
+import getpass
 from datetime import datetime
 
-print("Nome maquina:..........", platform.node())
-print("Arquitetura:...........", platform.architecture())
-print("Sistema Operacional:...", platform.system())
-print("Versao do SO:..........", platform.release())
-print("Processador:...........", platform.processor())
-print("Versao do Python:......", platform.python_version())
+# print("Nome maquina:..........", platform.node())
+# print("Arquitetura:...........", platform.architecture())
+# print("Sistema Operacional:...", platform.system())
+# print("Versao do SO:..........", platform.release())
+# print("Processador:...........", platform.processor())
+# print("Versao do Python:......", platform.python_version())
+#
+# print("\nAgora:...", datetime.now())
+# print("Ano:.....", datetime.now().year)
+# print("Hora:....", datetime.now().hour)
+# print("Mes:.....", datetime.now().month)
 
-print("\nAgora:...", datetime.now())
-print("Ano:.....", datetime.now().year)
-print("Hora:....", datetime.now().hour)
-print("Mes:.....", datetime.now().month)
+usuario = getpass.getuser()
+senha = getpass.getpass("Digite sua senha:...")
+
+if usuario == 'gedo' and senha == '123':
+    print("Bem vindo Gabriel")
+else:
+    print("Voce nao tem acesso")
+
